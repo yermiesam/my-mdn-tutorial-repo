77b3d286c65a42fa5178e9932c0672b878ac3c2e
yermiesam <y.s.santiago@outlook.com>
2022-03-07 16:20:12 -0500

    Added button and script

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,12 @@ const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 */
 
+
+// aka - ARROW FUNCTION
+document.querySelector('html').addEventListener('click', () => {
+    alert('Ouch! Stop poking me!');
+});
+
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
@@ -12,4 +18,26 @@ myImage.onclick = function() {
     } else {
         myImage.setAttribute('src', 'images/firefox-icon.png');
     }
+}
+
+let myButton = document.querySelector('button');
+
+myButton.onclick = function() {
+    setUserName();
+}
+
+
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName= prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent= 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')){
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent= 'Mozilla is cool, ' + storedName;
 }
